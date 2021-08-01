72ec037aa7c97354ea07d782eed4684f13aa7c34
조혜령 <chohyeryungcho@gmail.com>
2021-08-01 18:34:22 +0900

    Create _header, Build template, user test

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  root 'landing#index'
+  # devise_scope :user do
+  #   get '/login' => 'devise/sessions#new'
+  #   post '/login' => 'devise/session#create'
+  #   delete '/sign_out' => 'devise/session#destroy'
+  #   get '/sign_up' => 'devise/registrations#new'
+  #   post 'users' => 'devise/registrations#create'
+  #   get '/edit' => 'devise/registrations#edit'
+  #   patch '/update' => 'devise/registrations#update'
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
