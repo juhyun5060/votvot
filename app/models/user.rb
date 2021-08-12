71f1f4e8b00173e20f773f16b93780d4f31c3275
조혜령 <chohyeryungcho@gmail.com>
2021-08-12 15:18:30 +0900

    Add user profile to user!

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :posts
+         has_one_attached :profile
 
   # validates :nickname, :uniqueness => { :message => '이미 존재하는 닉네임입니다.' }
   # validates :nickname, :presence => { :message => '필수 입력사항입니다.' }
