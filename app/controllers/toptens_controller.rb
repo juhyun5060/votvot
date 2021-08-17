65fe964a56005937f210d2c3e7b970b7e6a1e1c1
조혜령 <chohyeryungcho@gmail.com>
2021-08-17 15:27:04 +0900

    topten ordering

--- app/controllers/toptens_controller.rb
+++ app/controllers/toptens_controller.rb
@@ -2,5 +2,13 @@ class ToptensController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    Post.find_each do |post|
+      @like = post.likes.count
+      puts Post.order(:@like).limit(5)
+    end
+    # puts Post.order(:Post.likes.count)
+    # @post = Post.find_by
+    # puts @post.likes.count
+    # @post = Post.order(:)
   end
 end
