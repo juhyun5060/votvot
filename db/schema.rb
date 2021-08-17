1db84869d6d418489c8cc4bd62f30c9e90b13951
조혜령 <chohyeryungcho@gmail.com>
2021-08-17 15:51:15 +0900

    post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_17_051613) do
+ActiveRecord::Schema.define(version: 2021_08_17_064511) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2021_08_17_051613) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
+    t.integer "likes_count"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -99,7 +100,6 @@ ActiveRecord::Schema.define(version: 2021_08_17_051613) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
-    t.integer "users"
     t.index ["post_id"], name: "index_subjects_on_post_id"
   end
 
