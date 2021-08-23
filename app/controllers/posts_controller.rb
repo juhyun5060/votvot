cfc8d7e7b1641453e0364b9f2104060b6499dfc3
조혜령 <chohyeryungcho@gmail.com>
2021-08-23 12:27:45 +0900

    post paging

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.paginate(page: params[:page], per_page:5)
+    @posts = Post.paginate(page: params[:page], per_page:15)
   end
 
   # GET /posts/1 or /posts/1.json
